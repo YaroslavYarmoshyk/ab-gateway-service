bfc5e69bc5ee12deecb47f536a3559a1506b572f
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2024-05-13 15:34:57 +0300

    First commit

---
+++ src/main/java/com/accommodationbooking/gatewayservice/AbGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.accommodationbooking.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/accommodationbooking/gatewayservice/AbGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accommodationbooking.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
